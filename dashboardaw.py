3e8ca7324d0b4845269b1d6cd8369edae16acb80
anisaherr <92285355+anisaherr@users.noreply.github.com>
2024-06-20 19:34:11 +0700

    Update dashboardaw.py

--- dashboardaw.py
+++ dashboardaw.py
@@ -1,28 +1,18 @@
-import os
 import streamlit as st
 import mysql.connector
 import pandas as pd
 import plotly.express as px
-import toml
 
 # Configure Streamlit page layout
 st.set_page_config(layout="wide", page_title="Adventure Works Dashboard")
 
-# Load secrets from the TOML file
-def load_secrets():
-    with open('secrets.toml', 'r') as f:
-        return toml.load(f)
-
-secrets = load_secrets()
-db_config = secrets['database']
-
 # Function to create a MySQL database connection
 def create_connection():
-    host = db_config.get('DB_HOST')
-    port = db_config.get('DB_PORT')
-    user = db_config.get('DB_USER')
-    password = db_config.get('DB_PASSWORD')
-    database = db_config.get('DB_DATABASE')
+    host = st.secrets["DB_HOST"]
+    port = st.secrets["DB_PORT"]
+    user = st.secrets["DB_USER"]
+    password = st.secrets["DB_PASSWORD"]
+    database = st.secrets["DB_DATABASE"]
 
     # Debugging print statements to check environment variables
     st.write(f"DB_HOST: {host}")
