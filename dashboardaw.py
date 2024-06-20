721d7253be83e80538b168c7360a13a58f714ba3
anisaherr <92285355+anisaherr@users.noreply.github.com>
2024-06-20 19:36:04 +0700

    Update dashboardaw.py

--- dashboardaw.py
+++ dashboardaw.py
@@ -8,36 +8,34 @@ st.set_page_config(layout="wide", page_title="Adventure Works Dashboard")
 
 # Function to create a MySQL database connection
 def create_connection():
-    host = st.secrets["DB_HOST"]
-    port = st.secrets["DB_PORT"]
-    user = st.secrets["DB_USER"]
-    password = st.secrets["DB_PASSWORD"]
-    database = st.secrets["DB_DATABASE"]
+    try:
+        host = st.secrets["DB_HOST"]
+        port = st.secrets["DB_PORT"]
+        user = st.secrets["DB_USER"]
+        password = st.secrets["DB_PASSWORD"]
+        database = st.secrets["DB_DATABASE"]
 
-    # Debugging print statements to check environment variables
-    st.write(f"DB_HOST: {host}")
-    st.write(f"DB_PORT: {port}")
-    st.write(f"DB_USER: {user}")
-    st.write(f"DB_PASSWORD: {password}")
-    st.write(f"DB_DATABASE: {database}")
+        if None in (host, port, user, password, database):
+            raise ValueError("One or more required environment variables are not set.")
 
-    if None in (host, port, user, password, database):
-        raise ValueError("One or more required environment variables are not set.")
-
-    return mysql.connector.connect(
-        host=host,
-        port=port,
-        user=user,
-        password=password,
-        database=database
-    )
+        return mysql.connector.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            database=database
+        )
+    except KeyError as e:
+        st.error(f"Key error: {e}")
+    except mysql.connector.Error as err:
+        st.error(f"Error: {err}")
 
 # Example usage
 try:
     conn = create_connection()
-    if conn.is_connected():
+    if conn and conn.is_connected():
         st.write("Connected to MySQL database successfully!")
-    # Perform operations with the connection
+        # Perform operations with the connection
 except mysql.connector.Error as err:
     st.write(f"Error: {err}")
 finally:
